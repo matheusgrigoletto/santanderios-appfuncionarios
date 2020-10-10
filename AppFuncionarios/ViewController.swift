bab9449f3a6a9667bc9b94ee087b916893ae6ebe
Matheus B. Grigoletto <matheus.grigoletto@me.com>
2020-10-10 08:45:38 -0300

    Initial Commit

---
+++ AppFuncionarios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppFuncionarios
+//
+//  Created by Matheus B. Grigoletto on 10/10/20.
+//  Copyright © 2020 Matheus B. Grigoletto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
